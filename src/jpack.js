4deece34c457fe64211c79dd382d0a6cd9fed9d2
HTMLGuyLLC <shane@htmlguy.com>
2019-07-12 17:48:59 -0400

    Fix jpack.js imports

--- src/jpack.js
+++ src/jpack.js
@@ -3,9 +3,4 @@ import * as objects from '../objects';
 import * as plugin_wrappers from '../plugin_wrappers';
 import * as utilities from '../utilities';
 
-export var jpack = {
-    components: components,
-    objects: objects,
-    plugin_wrappers: plugin_wrappers,
-    utilities: utilities
-};
+export {components, objects, plugin_wrappers, utilities};
