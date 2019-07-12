b49af7e94ea96fa107d9e92ae913d4742a280e75
HTMLGuyLLC <shane@htmlguy.com>
2019-07-12 18:07:17 -0400

    Fix jpack.js so that jpack is defined

--- src/jpack.js
+++ src/jpack.js
@@ -3,4 +3,4 @@ import * as objects from '../objects';
 import * as plugin_wrappers from '../plugin_wrappers';
 import * as utilities from '../utilities';
 
-export {components, objects, plugin_wrappers, utilities};
+export var jpack = {components, objects, plugin_wrappers, utilities};
